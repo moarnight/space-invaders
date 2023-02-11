26727b1ce8603780b4567743bdb9a15de655aedf
moarnight <moarnight@gmail.com>
2023-02-11 22:00:41 +0100

    Create background stars

--- index.js
+++ index.js
@@ -86,13 +86,14 @@ class Projectile {
 }
 
 class Particle {
-  constructor({ position, velocity, radius, color }) {
+  constructor({ position, velocity, radius, color, fades }) {
     this.position = position;
     this.velocity = velocity;
 
     this.radius = radius;
     this.color = color;
     this.opacity = 1;
+    this.fades = fades;
   }
 
   draw() {
@@ -112,7 +113,9 @@ class Particle {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
-    this.opacity -= 0.01;
+    if (this.fades) {
+      this.opacity -= 0.01;
+    }
   }
 }
 
@@ -259,7 +262,24 @@ const keys = {
 let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
 
-function createParticles({ object, color }) {
+for (let i = 0; i < 100; i++) {
+  particles.push(
+    new Particle({
+      position: {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+      },
+      velocity: {
+        x: 0,
+        y: 0.3,
+      },
+      radius: Math.random() * 2,
+      color: 'white',
+    })
+  );
+}
+
+function createParticles({ object, color, fades }) {
   for (let i = 0; i < 15; i++) {
     particles.push(
       new Particle({
@@ -273,6 +293,7 @@ function createParticles({ object, color }) {
         },
         radius: Math.random() * 3,
         color: color || 'rgb(188,68,239)',
+        fades,
       })
     );
   }
@@ -284,6 +305,11 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   particles.forEach((particle, i) => {
+    if (particle.position.y - particle.radius >= canvas.height) {
+      particle.position.x = Math.random() * canvas.width;
+      particle.position.y = -particle.radius;
+    }
+
     if (particle.opacity <= 0) {
       setTimeout(() => {
         particles.splice(i, 1);
@@ -319,6 +345,7 @@ function animate() {
       createParticles({
         object: player,
         color: 'white',
+        fades: true,
       });
     }
   });
@@ -364,6 +391,7 @@ function animate() {
             if (invaderFound && projectileFound) {
               createParticles({
                 object: invader,
+                fades: true,
               });
               grid.invaders.splice(i, 1);
               projectiles.splice(j, 1);
